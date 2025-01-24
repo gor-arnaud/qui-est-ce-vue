611fb1951ec9d95bf92bef9482dd35e3d8bf2002
gor-arnaud <arnaud.michoud@gameofroom.fr>
2025-01-24 10:30:07 +0100

    grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <ul>
-    <li v-for="person in persons" :key="person.id">
+  <div class="board">
+    <div class="criteria">
+      <h2>Critères</h2>
+    </div>
+    <div class="grid">
+      <div v-for="person in persons" :key="person.id">
       <img :src="require(`./assets/${person.id}.jpg`)">
-    </li>
-  </ul>
+    </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,5 +34,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.board {
+  display: flex;
+  flex-direction: column;
+}
+.grid {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-auto-columns: 1fr;
+  border: 1px solid #c1c1c1;
+  border-radius: 5px;
+  padding: 5px;
 }
 </style>
