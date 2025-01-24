5885f2a3be9bbfe5c99da8db251a18fdce85af76
gor-arnaud <arnaud.michoud@gameofroom.fr>
2025-01-24 11:11:14 +0100

    Aply filters

--- src/App.vue
+++ src/App.vue
@@ -4,37 +4,54 @@
       <h2>Critères</h2>
       <div class="criteriaList">
         <div v-for="criterion in criteria" :key="criterion">
-          {{  criterion }}
+          <select v-model="criterion.value">
+            <option
+              v-for="value in values"
+              :key="value.value"
+              :value="value.value"
+            >
+              {{ value.text }}
+            </option>
+          </select>
+          {{ criterion.id }}
         </div>
       </div>
     </div>
     <div class="grid">
       <div v-for="person in filteredPersons" :key="person.id">
-      <img :src="require(`./assets/${person.id}.jpg`)">
-    </div>
+        <img :src="require(`./assets/${person.id}.jpg`)" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import persons from './assets/persons.json';
+import persons from "./assets/persons.json";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       persons: persons,
-      selectedCriteria: []
-    }
+      criteria: [],
+      values: [
+        { text: "---", value: null },
+        { text: "Oui", value: true },
+        { text: "Non", value: false },
+      ],
+    };
   },
   computed: {
-    criteria() {
-      return Object.keys(persons[0]).filter(key => key !== 'id');
-    },
     filteredPersons() {
-      return persons.filter( p => {
-        for (let i = 0; i < this.selectedCriteria.length; i ++) {
-          const criterion = this.selectedCriteria[i];
+      return persons.filter((p) => {
+        for (let i = 0; i < this.criteria.length; i++) {
+          const criterion = this.criteria[i];
+
+          if (criterion.value === null) {
+            continue;
+          }
+
+          console.log("comparaison pour", p, criterion);
 
           if (p[criterion.id] != criterion.value) {
             return false;
@@ -43,11 +60,20 @@ export default {
 
         return true;
       });
-    }
+    },
   },
-  components: {
-  }
-}
+  components: {},
+  mounted() {
+    this.criteria = Object.keys(persons[0])
+      .filter((key) => key !== "id")
+      .map((key) => {
+        return {
+          id: key,
+          value: null,
+        };
+      });
+  },
+};
 </script>
 
 <style>
@@ -73,7 +99,7 @@ export default {
   padding: 5px;
 }
 .criteriaList {
-  display:flex;
+  display: flex;
   flex-direction: column;
 }
 </style>
